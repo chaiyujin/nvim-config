ed47b6f48b48d5052ffe49bcc6a363f939f92d3a
Yuki-Chai <504137683@qq.com>
2023-03-21 17:10:58 +0800

    Update init.lua (vscode-neovim)

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
-require("core.bootstrap").nvim_opt()
-require("core.bootstrap").basic_mapping()
-require("core.bootstrap").plugin()
+if vim.g.vscode then
+   -- vscode-neovim.
+   vim.cmd[[xmap gc  <Plug>VSCodeCommentary]]
+   vim.cmd[[nmap gc  <Plug>VSCodeCommentary]]
+   vim.cmd[[omap gc  <Plug>VSCodeCommentary]]
+   vim.cmd[[nmap gcc <Plug>VSCodeCommentaryLine]]
+else
+   -- neovim.
+   require("core.bootstrap").nvim_opt()
+   require("core.bootstrap").basic_mapping()
+   require("core.bootstrap").plugin()
+end
