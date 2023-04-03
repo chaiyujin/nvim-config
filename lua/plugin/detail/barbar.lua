ad541e09ce696e59930ea0c148fa98a79b938422
Yuki-Chai <504137683@qq.com>
2023-04-03 13:53:00 +0800

    Update barbar.lua

--- lua/plugin/detail/barbar.lua
+++ lua/plugin/detail/barbar.lua
@@ -43,23 +43,40 @@ M.opts = {
    -- Show every buffer
    hide = {current = false, inactive = false, visible = false},
 
-   -- Enable/disable icons
-   -- if set to 'numbers', will show buffer index in the tabline
-   -- if set to 'both', will show buffer index and icons in the tabline
-   icons = true,
+   icons = {
+      -- Configure the base icons on the bufferline.
+      buffer_index = false,
+      buffer_number = false,
+      button = '',
+      -- Enables / disables diagnostic symbols
+      diagnostics = {
+         [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+         [vim.diagnostic.severity.WARN] = {enabled = false},
+         [vim.diagnostic.severity.INFO] = {enabled = false},
+         [vim.diagnostic.severity.HINT] = {enabled = true},
+      },
+      filetype = {
+         -- Sets the icon's highlight group.
+         -- If false, will use nvim-web-devicons colors
+         custom_colors = false,
 
-   -- If set, the icon color will follow its corresponding buffer
-   -- highlight group. By default, the Buffer*Icon group is linked to the
-   -- Buffer* group (see Highlighting below). Otherwise, it will take its
-   -- default value as defined by devicons.
-   icon_custom_colors = false,
+         -- Requires `nvim-web-devicons` if `true`
+         enabled = true,
+      },
+      separator = {left = '▎', right = ''},
 
-   -- Configure icons on the bufferline.
-   icon_separator_active = '▎',
-   icon_separator_inactive = '▎',
-   icon_close_tab = '',
-   icon_close_tab_modified = '●',
-   icon_pinned = '車',
+      -- Configure the icons on the bufferline when modified or pinned.
+      -- Supports all the base icon options.
+      modified = {button = '●'},
+      pinned = {button = '車'},
+
+      -- Configure the icons on the bufferline based on the visibility of a buffer.
+      -- Supports all the base icon options, plus `modified` and `pinned`.
+      alternate = {filetype = {enabled = false}},
+      current = {buffer_index = true},
+      inactive = {button = '×'},
+      visible = {modified = {buffer_number = false}},
+   },
 
    -- If true, new buffers will be inserted at the start/end of the list.
    -- Default is to insert after current buffer.
