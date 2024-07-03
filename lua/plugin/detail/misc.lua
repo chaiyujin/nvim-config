cfdd53c9d636227c1c67496d74fb3cc05f358e67
Yuki-Chai <chaiyujin@gmail.com>
2024-07-03 23:13:35 +0800

    update gitsigns

--- lua/plugin/detail/misc.lua
+++ lua/plugin/detail/misc.lua
@@ -135,12 +135,22 @@ M.gitsigns = {
    dependencies = { "plenary.nvim" },
    opts = {
       signs = {
-         add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-         change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-         delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-         topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-         changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+         add          = { text = '┃' },
+         change       = { text = '┃' },
+         delete       = { text = '_' },
+         topdelete    = { text = '‾' },
+         changedelete = { text = '~' },
+         untracked    = { text = '┆' },
       },
+      signs_staged = {
+         add          = { text = '┃' },
+         change       = { text = '┃' },
+         delete       = { text = '_' },
+         topdelete    = { text = '‾' },
+         changedelete = { text = '~' },
+         untracked    = { text = '┆' },
+      },
+      signs_staged_enable = true,
       signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
       numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
       linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -169,9 +179,6 @@ M.gitsigns = {
          relative = 'cursor',
          row = 0,
          col = 1
-      },
-      yadm = {
-         enable = false
       },
    },
    config = function(_, opts)
