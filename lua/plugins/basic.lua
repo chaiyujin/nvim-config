c8bc7d17eb5b2b8dae2946deee43b63f56a85adc
Yujin Chai <chaiyujin@gmail.com>
2026-02-02 15:24:59 +0000

    lsp

--- lua/plugins/basic.lua
+++ lua/plugins/basic.lua
@@ -3,4 +3,17 @@ return {
    "nvim-lua/plenary.nvim",
    "nvim-tree/nvim-web-devicons",
    "folke/neodev.nvim",  -- for neovim lua development.
+
+   -- lsp config.
+   {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+         ensure_installed = { "lua_ls", "vimls", "pyright" },
+         automatic_enable = { "lua_ls", "vimls", "pyright" },
+      },
+      dependencies = {
+          { "mason-org/mason.nvim", opts = {} },
+          "neovim/nvim-lspconfig",
+      },
+   }
 }
